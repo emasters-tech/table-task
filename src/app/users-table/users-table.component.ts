801782252a96166ece09f7e7e9647cf316c099e7
elsawy269 <elsawy269@gmail.com>
2022-02-02 22:40:45 +0200

    fix: pagination

--- src/app/users-table/users-table.component.ts
+++ src/app/users-table/users-table.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { take } from 'rxjs/operators';
 import { PostDialogComponent } from '../post-dialog/post-dialog.component';
 import { Post } from '../shared/post.model';
 import { UserService } from '../user/user.service';
@@ -9,16 +12,24 @@ import { UserService } from '../user/user.service';
   templateUrl: './users-table.component.html',
   styleUrls: ['./users-table.component.scss'],
 })
-export class UsersTableComponent implements OnInit {
+export class UsersTableComponent implements OnInit  {
   constructor(private userService: UserService, public dialog: MatDialog) {}
-  
+  public dataSource ;
   public users$ = this.userService.users$;
 
   public displayedColumns: string[] = ['img','userName', 'title', 'body'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  public postsWithUser$ = this.userService.postsWithUser$;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.postsWithUser$.pipe(take(1)).subscribe(data=>{
+      this.dataSource=new MatTableDataSource<Post>(data);
+      debugger;
+      this.dataSource.paginator = this.paginator;
+
+    })
+  }
+
 
   public addPost(): void {
     this.dialog.open(PostDialogComponent, {
@@ -26,5 +37,4 @@ export class UsersTableComponent implements OnInit {
     });
   }
 
-  clickedRows = new Set<Post>();
 }
