a1e5a96bd3865973f80733cdc626dd55fff8b526
Mohamed Eldeeb <58711682+M-Hamo@users.noreply.github.com>
2022-02-02 15:55:26 +0200

    feat: adding style

--- src/app/users-table/users-table.component.ts
+++ src/app/users-table/users-table.component.ts
@@ -4,18 +4,16 @@ import { UserService } from '../user/user.service';
 @Component({
   selector: 'app-users-table',
   templateUrl: './users-table.component.html',
-  styleUrls: ['./users-table.component.scss']
+  styleUrls: ['./users-table.component.scss'],
 })
 export class UsersTableComponent implements OnInit {
-
   public dataSource: any;
-  public displayedColumns: string[] = ['img', 'name', 'email', 'city','phone'];
+  public displayedColumns: string[] = ['img', 'name', 'email', 'city', 'phone'];
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(users => {
+    this.userService.getUsers().subscribe((users) => {
       this.dataSource = users;
-    })
+    });
   }
-
 }
