24578c13d73cbed234d6c416b8e292a2441278d9
Mohamed Eldeeb <58711682+M-Hamo@users.noreply.github.com>
2022-02-02 15:53:15 +0200

    feat; install tailwend css

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public getUsers():Observable<User> {
-    return this.httpClient.get<User> ('https://jsonplaceholder.typicode.com/users');
+  public getUsers(): Observable<User> {
+    return this.httpClient.get<User>(
+      'https://jsonplaceholder.typicode.com/users'
+    );
   }
 }
