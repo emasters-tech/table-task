3420a6351bd10e368ea022454db9f5894a76d6cf
omarEMasters <omar@emasters.tech>
2022-02-02 20:16:26 +0200

    feat: add user

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { forkJoin, Observable, throwError } from 'rxjs';
+import { map  } from 'rxjs/operators';
 import { User } from './user.model';
 import { environment } from 'src/environments/environment';
 import { Post } from '../shared/post.model';
@@ -14,6 +15,15 @@ export class UserService {
   public users$: Observable<User[]> =this.httpClient.get<User[]>(`${environment.apiBaseUrl}/users`);
 
   public posts$: Observable<Post[]> =this.httpClient.get<Post[]>( `${environment.apiBaseUrl}/posts`);
+
+  public postsWithUser$: Observable<Post[]>=forkJoin(this.posts$,this.users$).pipe(map(([posts,users])=>
+     posts.map(post=>
+        ({
+         ...post,
+         userName:users.find(user=>post.userId===user.id)?.name
+       }) as Post
+     )
+  ))
   
   public submitPost(
     post: Post
--- src/app/users-table/users-table.component.ts
+++ src/app/users-table/users-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PostDialogComponent } from '../post-dialog/post-dialog.component';
+import { Post } from '../shared/post.model';
 import { UserService } from '../user/user.service';
 
 @Component({
@@ -9,18 +10,21 @@ import { UserService } from '../user/user.service';
   styleUrls: ['./users-table.component.scss'],
 })
 export class UsersTableComponent implements OnInit {
-
   constructor(private userService: UserService, public dialog: MatDialog) {}
-  public users$=this.userService.users$;
-  public displayedColumns: string[] = ['img', 'name', 'email', 'city', 'phone'];
+  
+  public users$ = this.userService.users$;
 
-  ngOnInit(): void {
- 
-  }
+  public displayedColumns: string[] = ['img','userName', 'title', 'body'];
+
+  public postsWithUser$ = this.userService.postsWithUser$;
+
+  ngOnInit(): void {}
 
   public addPost(): void {
     this.dialog.open(PostDialogComponent, {
       width: '500px',
     });
   }
+
+  clickedRows = new Set<Post>();
 }
